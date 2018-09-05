a8c051b541833727f4702fbf99300e2593fc5236
VotiveMobile Dilip <votivemobile.dilip@gmail.com>
2018-09-05 11:07:04 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/SquadFragment.java
@@ -0,0 +1,15 @@
+package com.androiddevs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SquadFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.squad_layout,null);
+    }
+}
---
+++ app/src/main/java/com/androiddevs/TableFragment.java
@@ -0,0 +1,16 @@
+package com.androiddevs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TableFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.table_layout,null);
+    }
+}
